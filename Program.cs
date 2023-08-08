0d9935dc83df22633c1edb5c1d1a3fd66c57b9d6
Rozikov Dilmurod <2110115@newuzbekistanuniversity.uz>
2023-08-08 23:15:12 +0500

    INFRA: Upload project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//KESCHA Project
+System.Console.WriteLine("Please, Enter your name:");
+string name = Console.ReadLine();
+System.Console.WriteLine($"Hello, {name}!");
+System.Console.WriteLine("Please, Enter your age:");
+int age = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"{age} is nice age!");
+int keschaAge = 12;
+System.Console.WriteLine("Age diffenece is " + (age - keschaAge));
+
+
