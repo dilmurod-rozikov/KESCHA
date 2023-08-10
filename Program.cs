c722aedfa760b7ebcd56bce3ca4ab5a6e9e6886b
Rozikov Dilmurod <2110115@newuzbekistanuniversity.uz>
2023-08-10 10:07:42 +0500

    logical operators

--- Program.cs
+++ Program.cs
@@ -6,15 +6,21 @@ System.Console.WriteLine("Please, Enter your age:");
 int age = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine($"{age} is nice age!");
 
+string message = age >= 0 
+        ? "age is positive!!!"
+        : "age is negative!!!";
+System.Console.WriteLine(message);
+
 int keschaAge = 12;
 System.Console.WriteLine("Age diffenece is " + (age - keschaAge));
 
-if(age > keschaAge){
-    System.Console.WriteLine("You are older!");
-} else if(age == keschaAge){
-    System.Console.WriteLine("You are equal!");
-}else{
-    System.Console.WriteLine("You are younger!");
+message = age >= keschaAge 
+        ? "You are older than or equal to KESCHA"
+        : "You are younger than KESCHA";
+System.Console.WriteLine(message)
+
+if(age - keschaAge >= 0 || age <= 0){
+    System.Console.WriteLine("Your age is less than KESCHA's age but positive.");
 }
 // System.Console.WriteLine("You are older than KASCHA " + (age > keschaAge));
 // System.Console.WriteLine("You are younger than KASCHA " + (age < keschaAge));
